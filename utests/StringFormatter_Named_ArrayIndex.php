2601ab2af7bcb65e98901ba54a9849aab3c73954
Marcin Sztolcman <marcin@urzenia.net>
2010-12-31 01:38:43 +0100

    added unit tests for mode: named, case: array index

---
+++ utests/StringFormatter_Named_ArrayIndex.php
@@ -0,0 +1,35 @@
+<?php
+
+error_reporting (E_ALL | E_STRICT);
+ini_set ('display_errors', true);
+
+require_once dirname (__FILE__) . '/simpletest/autorun.php';
+require_once dirname (dirname (__FILE__)) .'/StringFormatter.php';
+
+class StringFormatter_Named_ArrayIndex extends UnitTestCase {
+    public function __construct () {
+        parent::__construct ('Mode: named, case: array index');
+    }
+
+    public function testIndexNumericDoesNotExists () {
+        $sf = new StringFormatter ('test: "{val1[1]}"');
+        $this->assertEqual ($sf->parseNamed (array ('val1' => array ('Hello'))), 'test: "{val1[1]}"');
+    }
+
+    public function testIndexNumericExists () {
+        $sf = new StringFormatter ('test: "{val1[0]}"');
+        $this->assertEqual ($sf->parseNamed (array ('val1' => array ('Hello'))), 'test: "Hello"');
+    }
+
+    public function testKeyDoesNotExists () {
+        $sf = new StringFormatter ('test: "{val1[world]}"');
+        $this->assertEqual ($sf->parseNamed (array ('val1' => array ('hello' => 'Hello'))), 'test: "{val1[world]}"');
+    }
+
+    public function testKeyExists () {
+        $sf = new StringFormatter ('test: "{val1[hello]}"');
+        $this->assertEqual ($sf->parseNamed (array ('val1' => array ('hello' => 'Hello'))), 'test: "Hello"');
+    }
+
+}
+
